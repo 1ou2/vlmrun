039c13facc6a38aa2bd0d869c790bd4b25123057
gabriel pastor <gabriel@1ou2.com>
2025-07-01 23:04:03 +0200

    --

--- qwen_quantization.py
+++ qwen_quantization.py
@@ -23,8 +23,8 @@ model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
     device_map="auto"
 )
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct")
-
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct",use_fast=True)
+processor.save_pretrained("updated_processor")
 # The default range for the number of visual tokens per image in the model is 4-16384.
 # You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
 # min_pixels = 256*28*28
